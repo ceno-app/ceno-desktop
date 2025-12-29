ab8f6beb0df68b64efe70e52e988366cf372f6f4
Contemporary Code <28755351-contemporaryCode@users.noreply.gitlab.com>
2025-12-29 21:48:20 +0200

    [WIP] Add sys tray icon
    Todo:
    1) tray icon disappears instantly when exiting the program, but ouinet client is still wrapping it's things up. Make sure that there is no state where icon is gone, but ouinet client is still active.
    2) implement functionality to inherit already running instance of ceno network client
    3) maybe toggle to hide/show ouinet client.exe window? Easy to do at runtime, bit harder to hide already running window
    4) l10n for the ceno-network-client

---
+++ toolkit/ouinet/ceno-network-client/Resource.h
@@ -0,0 +1,10 @@
+#ifndef CENONETWORKCLIENT_RESOURCE_H
+#define CENONETWORKCLIENT_RESOURCE_H
+
+#define IDS_TOOLTIP             104
+#define IDI_NOTIFICATIONICON    207
+#define IDC_CONTEXTMENU         209
+#define IDM_EXIT                210
+#define IDM_OPEN                211
+
+#endif //CENONETWORKCLIENT_RESOURCE_H
