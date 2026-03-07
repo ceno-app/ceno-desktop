4d8990596c6bfe64094f02d404c5eb3a4c32c6ec
All Code is Contemporary Code <contemporarycode@pm.me>
2026-03-07 05:00:23 +0200

    Update Ouinet. Rework Ceno Network Client.
    Use DLL instead of wrapping client.exe.
    Use gray and colored tray icons to visualise current ouinet state.
    Report state in icon tooltip

---
+++ toolkit/ouinet/ceno-network-client/ArgumentsConverter.cpp
@@ -0,0 +1,84 @@
+#include <windows.h>
+#include <shellapi.h>
+
+#include "ArgumentsConverter.h"
+
+struct LocalFreeRAII {
+    LPWSTR* data;
+    explicit LocalFreeRAII(LPWSTR* data) : data(data) {}
+    ~LocalFreeRAII() {
+        if (data != NULL) {
+            LocalFree(data);
+        }
+    }
+};
+
+static std::optional<std::filesystem::path> getCenoNetworkClientExecutablePath() {
+    std::wstring moduleFilepathBuffer;
+    DWORD bufferLength = MAX_PATH;
+    while (true) {
+        moduleFilepathBuffer.resize(bufferLength, L'\0');
+
+        // Get C:\ceno-alpha\Ouinet\ceno-network-client.exe
+        if (0 == GetModuleFileName(nullptr, moduleFilepathBuffer.data(), bufferLength))
+            return {};
+
+        // Support for long filenames
+        if (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
+            if (bufferLength == 32768)
+                return {};
+            bufferLength = 32768;
+        } else {
+            // trim the string
+            moduleFilepathBuffer.resize(wcsnlen(moduleFilepathBuffer.c_str(), moduleFilepathBuffer.size()));
+            break;
+        }
+    }
+
+    return std::filesystem::path { moduleFilepathBuffer };
+}
+
+
+ArgvConverter::ArgvConverter(const wchar_t *wArgsStr) {
+    ceno_network_client_path = getCenoNetworkClientExecutablePath();
+    if (!ceno_network_client_path) {
+        return;
+    }
+
+    const LocalFreeRAII wArgv { CommandLineToArgvW(wArgsStr, &argc) };
+    if (NULL == wArgv.data) {
+        return;
+    }
+
+    storage.reserve(argc + 1);
+    argv.reserve(argc + 1);
+
+    storage.push_back(ceno_network_client_path.value().string());
+    argv.push_back(storage.back().c_str());
+
+    for (int i = 0; i < argc; ++i) {
+        if (constexpr std::wstring_view repoArgument(L"--repo"); repoArgument == wArgv.data[i] && i + 1 < argc) {
+            repo_path = std::filesystem::path(wArgv.data[i + 1]);
+        }
+
+        const int length = WideCharToMultiByte(CP_UTF8, 0, wArgv.data[i], -1, nullptr, 0, nullptr, nullptr);
+
+        // length includes null terminator
+        std::string narrow(length - 1, '\0');
+        WideCharToMultiByte(CP_UTF8, 0, wArgv.data[i], -1, narrow.data(), length, nullptr, nullptr);
+        storage.push_back(std::move(narrow));
+        argv.push_back(storage.back().c_str());
+    }
+    argc++;
+
+    // null terminate the argv array
+    argv.push_back(nullptr);
+}
+
+void ArgvConverter::cleanup() {
+    argc = 0;
+    storage.clear();
+    argv.clear();
+    repo_path.reset();
+    ceno_network_client_path.reset();
+}
--- toolkit/ouinet/ceno-network-client/GuiWindow.h
+++ toolkit/ouinet/ceno-network-client/GuiWindow.h
@@ -1,11 +1,16 @@
 #pragma once
 
-#include <filesystem>
+#include <atomic>
 
 #include <windows.h>
 
-HWND createGuiWindow(HINSTANCE hInstance, const std::filesystem::path &cenoExecutablePath);
-void requestWindowClose();
+#include "ArgumentsConverter.h"
+#include "ProcessIdFile.h"
 
-bool createNotificationIcon(HWND windowHandle);
-void removeNotificationIcon(HWND windowHandle);
+extern std::atomic<HWND> windowHandleForCommunicatingFromOtherThreads;
+extern std::atomic_int g_exitCode;
+extern std::atomic_flag g_ouinetIsStuckOnExit_ForceExitInMain;
+
+constexpr UINT ouinetStateChange = WM_APP + 3;
+
+HWND createGuiWindow(HINSTANCE hInstance, ArgvConverter *args, ProcessIdFile *pidFile);
