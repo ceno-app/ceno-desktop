d0f379f530b8566e135f96995c42b1b3feccf773
All Code is Contemporary Code <contemporarycode@pm.me>
2026-07-10 22:51:52 +0300

    BaseBrowser-140.12.0esr build break fixes
    clang-tidy download fails during bootstrap. Not actually used by Ceno Browser's build, no need to download then.
    RELATIVE_DATA_DIR also not defined by Ceno build process. Hardcode it

--- python/mozboot/mozboot/bootstrap.py
+++ python/mozboot/mozboot/bootstrap.py
@@ -361,7 +361,7 @@ class Bootstrapper:
         self.instance.install_toolchain_artifact("fix-stacks")
         self.instance.install_toolchain_artifact("minidump-stackwalk")
         if not self.instance.artifact_mode:
-            self.instance.install_toolchain_artifact("clang-tools/clang-tidy")
+            #self.instance.install_toolchain_artifact("clang-tools/clang-tidy")
             self.instance.ensure_sccache_packages()
         # Like 'ensure_browser_packages' or 'ensure_mobile_android_packages'
         getattr(self.instance, "ensure_%s_packages" % application)()
